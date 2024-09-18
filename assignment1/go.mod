ec551c98f33a8fb2ae567a28437aec446b28afca
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2024-09-17 19:26:33 -0700

    init commit, assignment 1 wip

---
+++ assignment1/go.mod
@@ -0,0 +1,3 @@
+module cpsc449-assignments/assignment1
+
+go 1.23.1
---
+++ assignment1/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"html"
+	"net/http"
+	"strings"
+)
+
+type Person struct {
+	Name   string `json:"name"`
+	School string `json:"school"`
+	OS     string `json:"os"`
+	Editor string `json:"editor"`
+	Lang   string `json:"lang"`
+}
+
+func main() {
+	// 1st requirement
+	http.HandleFunc("/hello/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		}
+		fmt.Fprintf(w, "Hello! My name is %q", html.EscapeString(r.URL.Path[strings.LastIndex(r.URL.Path, "/")+1:]))
+	})
+
+	// 2nd requirement
+	http.HandleFunc("/data", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		}
+
+		var person Person
+		err := json.NewDecoder(r.Body).Decode(&person)
+		if err != nil {
+			http.Error(w, "Bad Request", http.StatusBadRequest)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+
+		res, err := json.Marshal(person)
+		if err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		}
+
+		w.Write(res)
+	})
+
+	fmt.Println("Starting server on port 8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+}
